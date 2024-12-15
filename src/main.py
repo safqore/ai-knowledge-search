a60174151c9d0a79dcb4fbe078597c02671dd85b
Abbad Minhas <abbad.minhas@gmail.com>
2024-12-15 23:57:25 +0000

    Added Embeddings and Vector Store

--- src/main.py
+++ src/main.py
@@ -13,55 +13,86 @@ LANGCHAIN_PROJECT="ai-knowledge-search"
 os.environ["LANGSMITH_API_KEY"] = os.getenv('LANGSMITH_API_KEY')
 os.environ["LANGSMITH_TRACING"] = "true"
 
-# # Load PDF document via Langchain loaders - https://python.langchain.com/docs/how_to/document_loader_pdf/
-# from langchain_community.document_loaders import PyPDFLoader
-# str_pdf_path = "data/national-cdf-list-v1.331.pdf"
-# obj_loader = PyPDFLoader(str_pdf_path)
-# lst_documents = obj_loader.load()
+# Load PDF document via Langchain loaders - https://python.langchain.com/docs/how_to/document_loader_pdf/
+from langchain_community.document_loaders import PyPDFLoader
+str_pdf_path = "data/national-cdf-list-v1.331.pdf"
+obj_loader = PyPDFLoader(str_pdf_path)
+lst_documents = obj_loader.load()
 
-# # Extract all PDF content 
-# lst_pdf_content = [obj_document.page_content for obj_document in lst_documents]
-# str_pdf_content = " ".join(lst_pdf_content)
-# # print(str_pdf_content)
+# Extract all PDF content 
+lst_pdf_content = [obj_document.page_content for obj_document in lst_documents]
+str_pdf_content = " ".join(lst_pdf_content)
+# print(str_pdf_content)
 
-# # Split PDF content into chunks for downsteam LLM processing - https://python.langchain.com/docs/concepts/text_splitters/
-# # See for chunking visualisation: https://chunkviz.up.railway.app/
-# from langchain_text_splitters import RecursiveCharacterTextSplitter
-# obj_text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-# lst_chunks = obj_text_splitter.split_text(str_pdf_content)
-# # print(lst_chunks[0])
+# Split PDF content into chunks for downsteam LLM processing - https://python.langchain.com/docs/concepts/text_splitters/
+# See for chunking visualisation: https://chunkviz.up.railway.app/
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+obj_text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+lst_chunks = obj_text_splitter.split_text(str_pdf_content)
+# print(lst_chunks[0])
 
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+# Create embeddings for the text chunks
+from langchain_huggingface import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv('HUGGING_FACE_HUB_TOKEN')
+from langchain_community.vectorstores import FAISS
+from faiss import IndexFlatL2
+from langchain.docstore import InMemoryDocstore
 
-obj_llm = HuggingFaceEndpoint(
-    repo_id="microsoft/Phi-3-mini-4k-instruct",
-    task="text-generation",
-    max_new_tokens=512,
-    do_sample=False,
-    repetition_penalty=1.03,
-)
+dimension = 768  # This should match the dimension of your embeddings
+index = IndexFlatL2(dimension)
+docstore = InMemoryDocstore({})
+index_to_docstore_id = {}
+vector_store = FAISS(embedding_function=embeddings, index=index, docstore=docstore, index_to_docstore_id=index_to_docstore_id)
+text_ids = vector_store.add_texts(lst_chunks)
 
-chat_model = ChatHuggingFace(llm=obj_llm)
+print(text_ids[:3])
 
-from langchain_core.messages import (
-    HumanMessage,
-    SystemMessage,
-)
+# from langchain.vectorstores import FAISS
+# vector_store = FAISS.from_texts(lst_chunks, embeddings)
 
-messages = [
-    SystemMessage(content="You're a helpful assistant"),
-    HumanMessage(
-        content="What happens when an unstoppable force meets an immovable object?"
-    ),
-]
+# # Search for a keyword or phrase
+# query = "Alectinib"
+# results = vector_store.similarity_search(query)
 
-ai_msg = chat_model.invoke(messages)
+# # Print the search results
+# for result in results:
+#     print(result.page_content)
 
-print(ai_msg.content)
 
-# # Summarize the text chunks
+# from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+# os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv('HUGGING_FACE_HUB_TOKEN')
+
+# obj_llm = HuggingFaceEndpoint(
+#     repo_id="microsoft/Phi-3-mini-4k-instruct",
+#     task="text-generation",
+#     max_new_tokens=512,
+#     do_sample=False,
+#     repetition_penalty=1.03,
+# )
+
+# chat_model = ChatHuggingFace(llm=obj_llm)
+
+# from langchain_core.messages import (
+#     HumanMessage,
+#     SystemMessage,
+# )
+
+# messages = [
+#     SystemMessage(content="You're a helpful assistant"),
+#     HumanMessage(
+#         content="What happens when an unstoppable force meets an immovable object?"
+#     ),
+# ]
+
+# ai_msg = chat_model.invoke(messages)
+
+# print(ai_msg.content)
+
+# ############################
+
+# Summarize the text chunks
 # from langchain_core.prompts import PromptTemplate
 # from langchain_core.runnables import RunnableSequence
 # question_prompt = PromptTemplate.from_template(
@@ -74,18 +105,3 @@ print(ai_msg.content)
 # # Print the summaries
 # for summary in summaries:
 #     print(summary)
-
-# # Create embeddings for the text chunks
-# from langchain.embeddings import HuggingFaceEmbeddings
-# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
-# from langchain.vectorstores import FAISS
-# vector_store = FAISS.from_texts(lst_chunks, embeddings)
-
-# # Search for a keyword or phrase
-# query = "Alectinib"
-# results = vector_store.similarity_search(query)
-
-# # Print the search results
-# for result in results:
-#     print(result.page_content)
